1bc640b770a7036e23f74278dd4f9779b5a3be75
danlindleybaker <danlindleybaker@gmail.com>
2023-09-15 13:13:57 +0100

    added more output file settings

--- smponpol/ui.py
+++ smponpol/ui.py
@@ -12,14 +12,25 @@ class SMPonpolUI:
 class OutputFileWindow:
     def __init__(self):
         with dpg.window(label="Output File Settings",
-                        pos=[0,0],
+                        pos=[0, 0],
                         width=VIEWPORT_WIDTH/2,
                         height=VIEWPORT_HEIGHT/4):
             with dpg.group(horizontal=True):
-                dpg.add_text("Folder: ")
-                self.output_folder = dpg.add_input_text(default_value=".\\Data")
+                dpg.add_text(f"{'Folder':>15}: ")
+                self.output_folder = dpg.add_input_text(
+                    default_value=".\\Data")
                 dpg.add_button(label="Browse",
                                callback=lambda: dpg.show_item("folder_dialog"))
+            with dpg.group(horizontal=True):
+                dpg.add_text(f"{'Sample Name':>15}: ")
+                self.sample_name = dpg.add_input_text(default_value="Sample 1")
+            with dpg.group(horizontal=True):
+                dpg.add_text(f"{'Cell Type':>15}: ")
+                self.sample_name = dpg.add_input_text(default_value="HG")
+            with dpg.group(horizontal=True):
+                dpg.add_text(f"{'Cell Thickness':>15}: ")
+                self.sample_name = dpg.add_input_text(default_value="5um")
+
 
         dpg.add_file_dialog(
             directory_selector=True,
@@ -29,8 +40,8 @@ class OutputFileWindow:
             id="folder_dialog",
             width=700,
             height=400
-            )
-            
+        )
+
 
 def saveas_folder_callback(sender, app_data, output_file_path):
     dpg.set_value(output_file_path, app_data["file_path_name"])
