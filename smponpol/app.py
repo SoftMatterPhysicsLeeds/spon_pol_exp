b63af9a7b932461616534e68a8453c18bfd23102
danlindleybaker <danlindleybaker@gmail.com>
2023-09-15 13:06:22 +0100

    added browse folder dialog

--- smponpol/app.py
+++ smponpol/app.py
@@ -1,11 +1,15 @@
 import dearpygui.dearpygui as dpg
-from smponpol.ui import VIEWPORT_WIDTH, DRAW_HEIGHT
-
+from smponpol.ui import VIEWPORT_WIDTH, DRAW_HEIGHT, SMPonpolUI
+from pathlib import Path
 
 def main():
-    print("hello!")
+    font_path = Path("./assets/IosevkaNerdFont-Regular.ttf")
     dpg.create_context()
 
+    with dpg.font_registry():
+        default_font = dpg.add_font(
+            font_path, 18)
+    dpg.bind_font(default_font)
     dpg.create_viewport(
         title="SMPontaneous Polarisation",
         width=VIEWPORT_WIDTH,
@@ -15,10 +19,13 @@ def main():
     dpg.setup_dearpygui()
     dpg.show_viewport()
 
+    ui = SMPonpolUI()
+
     while dpg.is_dearpygui_running():
         dpg.render_dearpygui_frame()
 
     dpg.destroy_context()
+
 
 if __name__ == "__main__":
     main()
--- smponpol/ui.py
+++ smponpol/ui.py
@@ -1,4 +1,36 @@
+import dearpygui.dearpygui as dpg
 VIEWPORT_WIDTH = 1280
 DRAW_HEIGHT = 850
 VIEWPORT_HEIGHT = DRAW_HEIGHT - 40
 
+
+class SMPonpolUI:
+    def __init__(self):
+        self.output_file_window = OutputFileWindow()
+
+
+class OutputFileWindow:
+    def __init__(self):
+        with dpg.window(label="Output File Settings",
+                        pos=[0,0],
+                        width=VIEWPORT_WIDTH/2,
+                        height=VIEWPORT_HEIGHT/4):
+            with dpg.group(horizontal=True):
+                dpg.add_text("Folder: ")
+                self.output_folder = dpg.add_input_text(default_value=".\\Data")
+                dpg.add_button(label="Browse",
+                               callback=lambda: dpg.show_item("folder_dialog"))
+
+        dpg.add_file_dialog(
+            directory_selector=True,
+            show=False,
+            callback=saveas_folder_callback,
+            user_data=self.output_folder,
+            id="folder_dialog",
+            width=700,
+            height=400
+            )
+            
+
+def saveas_folder_callback(sender, app_data, output_file_path):
+    dpg.set_value(output_file_path, app_data["file_path_name"])
