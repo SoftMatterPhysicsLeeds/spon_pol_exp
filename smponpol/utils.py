d4df3e7f2e76f7d1513909b10b9b9928a3e5294e
danlindleybaker <danlindleybaker@gmail.com>
2023-09-19 13:34:37 +0100

    added RIGOL connection to instrument control window

--- smponpol/utils.py
+++ smponpol/utils.py
@@ -19,6 +19,8 @@ def find_instruments(frontend: SMPonpolUI):
         frontend.instrument_control_window.linkam_com_selector, items=com_selector)
     dpg.configure_item(
         frontend.instrument_control_window.agilent_com_selector, items=usb_selector)
+    dpg.configure_item(
+        frontend.instrument_control_window.rigol_com_selector, items=usb_selector)
 
     # dpg.set_value(frontend.measurement_status, "Found instruments!")
     # dpg.set_value(frontend.measurement_status, "Idle")
